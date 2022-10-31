466f47dc4a4869094a39818c8cb3306353d52aea
Behzod <behzodtuit@gmail.com>
2022-10-31 16:10:41 +0500

    aiogram-template-bot

---
+++ handlers/users/help.py
@@ -0,0 +1,14 @@
+from aiogram import types
+from aiogram.dispatcher.filters.builtin import CommandHelp
+
+from loader import dp
+
+
+@dp.message_handler(CommandHelp())
+async def bot_help(message: types.Message):
+    text = ("Qanday yordam kerak?",
+            "Buyruqlar: ",
+            "/start - Botni ishga tushirish",
+            "/help - Yordam")
+    
+    await message.answer("\n".join(text))
