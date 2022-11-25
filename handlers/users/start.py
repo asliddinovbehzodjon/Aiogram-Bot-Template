7dba45b586c898c2fdbeb32c05bc01f8d87b0fe5
Behzod <behzodtuit@gmail.com>
2022-11-25 18:16:51 +0500

    db-template

---
+++ handlers/users/start.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+from aiogram import types
+from aiogram.dispatcher.filters.builtin import CommandStart
+
+from data.config import ADMINS
+from loader import dp, db, bot
+
+
+@dp.message_handler(CommandStart())
+async def bot_start(message: types.Message):
+    name = message.from_user.full_name
+    # Foydalanuvchini bazaga qo'shamiz
+    try:
+        db.add_user(id=message.from_user.id,
+                    name=name)
+    except sqlite3.IntegrityError as err:
+        await bot.send_message(chat_id=ADMINS[0], text=err)
+
+    await message.answer("Xush kelibsiz!")
+    # Adminga xabar beramiz
+    count = db.count_users()[0]
+    msg = f"{message.from_user.full_name} bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor."
+    await bot.send_message(chat_id=ADMINS[0], text=msg)
